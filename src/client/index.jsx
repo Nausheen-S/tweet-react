3c12c701b470c9e6e42ac3a08fc4c57862af1fde
Nausheen_S <salauddinnausheen@gmail.com>
2020-05-28 22:48:07 +0800

    completed MVP

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,20 +1,148 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 
-import tweets from 'tweets'
+ import tweets from 'tweets'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        tweets
-      </div>
-    );
-  }
+ class User extends React.Component{
+    render() {
+     return (
+       <div>
+        {this.props.user}
+       </div>
+     );
+ }
+ }
+ class Entities extends React.Component{
+    render() {
+     return (
+       <div>
+       <UrlEntity urls={this.props.entity.urls}/>
+       </div>
+     );
+ }
+ }
+ class UrlEntity extends React.Component{
+        render() {
+                 console.log(this.props.url);
+         let urlsEntitity = this.props.urls.map(url=>{
+             return <a href={url.url}>{url.url}</a>
+         })
+     return (
+       <div>
+     {urlsEntitity}
+       </div>
+     );
+ }
+ }
+ class Tweet extends React.Component{
+       render() {
+     let tweets = this.props.tweets.map(tweet=>{
+         return (<li>
+             <User user={tweet.user.screen_name}/>
+             <p>{tweet.text}</p>
+            <Entities entity={tweet.entities}/>
+             </li>)
+     });
+     return (
+       <div>
+        <ul> {tweets}</ul>
+       </div>
+     );
+ }
+ }
+
+ class App extends React.Component {
+   render() {
+
+     return (
+       <div>
+         tweets
+       <Tweet tweets={tweets.tweets}/>
+       </div>
+     );
+   }
+}
+ const element = document.getElementById('app');
+
+ ReactDOM.render(<App />, element );
+
+ console.log("tweet react");
+ */
+
+ import React from 'react';
+import ReactDOM from 'react-dom';
+
+ import tweets from 'tweets';
+
+
+
+class User extends React.Component {
+   render() {
+
+     return (
+       <div>
+         {this.props.user}
+       </div>
+     );
+   }
 }
 
-const element = document.getElementById('app');
+class Entity extends React.Component{
+    render() {
+     return (
+       <div>
+       <Url urls={this.props.entity.urls}/>
+       </div>
+     );
+ }
+ }
 
-ReactDOM.render(<App />, element );//
+ class Url extends React.Component{
+    render() {
+        let urlsEntity = this.props.urls.map(url=>{
+             return <div>{url.url}</div>
+         })
+     return (
+       <div>
+       {urlsEntity}
+       </div>
+     );
+ }
+ }
 
-console.log("tweet react");
+class Tweet extends React.Component {
+   render() {
+    let tweets = this.props.tweets.map(tweet=>{
+        return (
+            <li><User user={tweet.user.screen_name}/>
+            <Entity entity={tweet.entities}/></li>
+
+            )
+    });
+
+    return (
+          <ul>
+            {tweets}
+          </ul>
+        );
+
+
+   }
+}
+
+
+
+ class App extends React.Component {
+   render() {
+    return (
+       <div>
+      <Tweet tweets = {tweets.tweets}/>
+      </div>
+     );
+   }
+}
+ const element = document.getElementById('app');
+
+ ReactDOM.render(<App />, element );
+
+ console.log("tweet react");
